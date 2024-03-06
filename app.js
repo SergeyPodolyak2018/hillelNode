0b91ebed38ed14ae2e251bc2162a30c31dfe9667
Sergey Podolyak <sergey.dev.2024@gmail.com>
2024-03-06 20:41:34 +0200

    Home work 5

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ import logger from './lib/logger/logger.js';
 
 import color from './color.js';
 import fruit from './fruit.js';
-import { add } from './handler.js';
 
 const log = logger.getLogger('app.js');
 
 log.info(color);
-//log.info(fruit, color);
-// log.error('ERROR occur: My log');
+log.error(color);
 log.debug(fruit);
-// log.trace(fruit);
-
-// add(3, 5);
